b2a6154206a385ad8993907531b3e7796a9b0acc
Lancelot Da Costa <41574157+lancelotdacosta@users.noreply.github.com>
2024-11-14 17:55:48 +0100

    learning of D seems to work

--- examples/my_demos/simplest_learning_demo.py
+++ examples/my_demos/simplest_learning_demo.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# #  Demo: Simplest Environment + Learning of transition probabilities
+#  Demo: Simplest Environment + Learning of contingencies
 # In[1]:
 
 import copy
 import os
 import pathlib
 import sys
-from array import array
 
 import numpy as np
 
@@ -22,43 +18,57 @@ from pymdp.utils import plot_likelihood
 from pymdp import utils
 
 # In[]:
-# reward_probabilities = [0.85, 0.15]  # the 'true' reward probabilities
 env = SimplestEnv()
 A_gp = env.get_likelihood_dist()
 B_gp = env.get_transition_dist()
 
 # In[]:
-learning_A = True # set to True if you want to learn the observation probabilities
+def update_matrix(agent, update_method_name, history_list, learning, *args):
+    """
+    Conditionally updates a matrix if learning is enabled, using the specified agent method,
+    and appends the result to the given history list.
 
-if learning_A:
-    pA = copy.deepcopy(A_gp) #make a copy of the likelihoods for initialization of dirichlet array of right shape
-    cA = 1 # this is the concentration parameter of the Dirichlet distribution. It is a scalar that determines the strength of the prior. A high value means the prior is strong, a low value means the prior is weak.
-    for i in range(len(pA)): #make each entry uniform
-        pA[i] = cA * np.ones_like(pA[i])
-    A_gm = utils.norm_dist_obj_arr(pA)
+    Parameters:
+    - agent: the agent object with update methods for A, B, and D.
+    - update_method_name: name of the method (string) to be called for updating (e.g., "update_A").
+    - history_list: list to append the update result to (e.g., pA_history, pB_history, pD_history).
+    - learning: boolean, whether to perform the update.
+    - *args: additional arguments to pass to the update method.
+    """
+    if learning:
+        update_method = getattr(agent, update_method_name)
+        update = update_method(*args)
+        history_list.append(update)
 
-else:
-    A_gm = copy.deepcopy(A_gp)
-    pA = None
-
-# plot_likelihood(A_gm[0][:, :], 'Likelihood map \n from location state to location observation')
 
 # In[]:
-learning_B = True # set to True if you want to learn the transition probabilities
+def init_distribution(learning_enabled, base_dist, concentration=1):
+    'initialises dirichlet prior with uniform parameters if learning is enabled, otherwise returns the base distribution'
+    if learning_enabled:
+        p = copy.deepcopy(base_dist)
+        for i in range(len(p)):
+            p[i] = concentration * np.ones_like(p[i])
+        return utils.norm_dist_obj_arr(p), p
+    return copy.deepcopy(base_dist), None
 
-if learning_B:
-    pB = copy.deepcopy(B_gp) #make a copy of the likelihoods for initialization of dirichlet array of right shape
-    cB = 1 # this is the concentration parameter of the Dirichlet distribution. It is a scalar that determines the strength of the prior. A high value means the prior is strong, a low value means the prior is weak.
-    for i in range(len(pB)): #make each entry uniform
-        pB[i] = cB * np.ones_like(pB[i])
-    B_gm = utils.norm_dist_obj_arr(pB)
+# Initialise the likelihood and transition distributions of the generative model
+learning_A=False
+A_gm, pA = init_distribution(learning_enabled=learning_A, base_dist= A_gp)
+learning_B=False
+B_gm, pB = init_distribution(learning_enabled=learning_B, base_dist= B_gp)
+learning_D=True
+D_gm, pD = init_distribution(learning_enabled=learning_D, base_dist= utils.obj_array_uniform(env.num_states))
 
-else:
-    B_gm = copy.deepcopy(B_gp)
-    pB = None
-
+# In[]:
+# plot_likelihood(A_gm[0][:, :], 'Likelihood map \n from location state to location observation')
 # plot_likelihood(B_gm[0][:, :, 0], 'Transition from location states under action LEFT')
 # plot_likelihood(B_gm[0][:, :, 1], 'Transition from location states under action RIGHT')
+
+# In[9]:
+
+# agent.D[0] = utils.onehot(0, agent.num_states[0])  # set the initial prior over location state to be dirac at left location
+#agent.D = utils.obj_array_uniform(agent.num_states) # set the initial prior over location state to be uniform
+#agent.D[0] = np.array([0.51, 0.49])  # set the initial prior over location state to be uniform
 
 # In[7]:
 
@@ -71,29 +81,31 @@ controllable_indices = [0]  # this is a list of the indices of the hidden state 
 # In[8]:
 
 
-agent = Agent(A=A_gm, pA=pA, B=B_gm, pB=pB,
+agent = Agent(A=A_gm, pA=pA, B=B_gm, pB=pB, D=D_gm, pD=pD,  # pass the likelihood, transition, and initial state probability distributions
               control_fac_idx=controllable_indices,
               # modalities_to_learn=learnable_modalities,   #commented as default is to learn all modalities
               # factors_to_learn=learnable_factors,         #commented as default is to learn all factors
-              lr_pA=1,lr_pB=1,  # mathematically this should always be equal to 1.0
+              lr_pA=1,lr_pB=1,lr_pD=1,  # mathematically this should always be equal to 1.0
+              save_belief_hist=True,  # this saves the inferences about states and policies over time: in agent.q_pi_hist and agent.qs_hist
               use_param_info_gain=True)
 
 # see how this compares to the non-learning case: agent = Agent(A=A_gm, B=B_gm, control_fac_idx=controllable_indices)
 
 # In[9]:
 
-agent.D[0] = utils.onehot(0, agent.num_states[
-    0])  # set the initial prior over location state to be dirac at left location
+# agent.D[0] = utils.onehot(0, agent.num_states[0])  # set the initial prior over location state to be dirac at left location
+#agent.D = utils.obj_array_uniform(agent.num_states) # set the initial prior over location state to be uniform
+#agent.D[0] = np.array([0.51, 0.49])  # set the initial prior over location state to be uniform
 
 # In[10]:
 
-# set the prior over observations
+# set the unnormalised log prior over observations
 agent.C[0][0] = 0.0
 agent.C[0][1] = 0.0
 
 # In[11]:
 
-T = 10  # number of timesteps
+T = 4  # number of timesteps
 
 obs = env.reset()  # reset the environment and get an initial observation
 
@@ -102,24 +114,22 @@ location_observations = ['Left','Right']
 msg = """ === Starting experiment === \n Observation: [{}]"""
 print(msg.format(location_observations[obs[0]]))
 
-pA_history, pB_history, qx_history, q_pi_history = [], [], [], []
+pA_history, pB_history, pD_history = [pA], [pB], [pD]
 
 all_actions = np.zeros((T, 1))  # 1 because there is one state factor.
 
 # state inference
 qx = agent.infer_states(obs)
-qx_history.append(qx)
+print('Initial state inference: Left', qx[0][0], 'Right', qx[0][1])
 
-# parameter learning of matrix A
-if learning_A:
-    pA_t = agent.update_A(obs)  # Update approximate posterior beliefs about Dirichlet parameters. Note that we do so after each observation, not at the end of the tria
-    pA_history.append(pA_t)
+# Call the helper function for each matrix with the appropriate learning flag
+update_matrix(agent, "update_A", pA_history, learning_A, obs) # parameter learning of matrix A
+update_matrix(agent, "update_D", pD_history, learning_D)
 
 for t in range(T):
     # policy inference
     q_pi, efe = agent.infer_policies()
-    q_pi_history.append(q_pi)
-    if t==0: print('Initial efe:', efe)
+    #if t==0: print('Initial efe:', efe)
 
     # action selection
     action = agent.sample_action()  # the action is a vector that gives the action index for each controllable state factor. in this case it is a vector of two numbers.
@@ -136,26 +146,27 @@ for t in range(T):
 
     # state inference
     qx = agent.infer_states(obs)
-    qx_history.append(qx)
+    print(f'[Step {t}] State inference: Left {qx[0][0]}, Right {qx[0][1]}')
 
-    if learning_A:    # parameter learning of matrix A
-        pA_t = agent.update_A(obs)
-        pA_history.append(pA_t)
-
-    if learning_B:    # parameter learning of matrix B
-        pB_t = agent.update_B(qx_history[t-1])
-        pB_history.append(pB_t)
+    # Call the helper function for each matrix with the appropriate learning flag
+    update_matrix(agent, "update_A", pA_history, learning_A, obs)  # parameter learning of matrix A
+    update_matrix(agent, "update_B", pB_history, learning_B, agent.qs_hist[t - 1]) # parameter learning of matrix B
+    update_matrix(agent, "update_D", pD_history, learning_D) # parameter learning of matrix D
 
 # In[12]:
 
 if learning_A:
-    #plot_likelihood(A_gm[0][:, :], 'Initial beliefs about location->observation mapping')
+    # plot_likelihood(A_gm[0][:, :], 'Initial beliefs about location->observation mapping')
     plot_likelihood(agent.A[0][:, :], 'Final beliefs about location->observation mapping') # recall that when learning, agent.A is the expected value of the approximate posterior over A.
-    #plot_likelihood(A_gp[0][:, :], 'True location->observation mapping')
+    # plot_likelihood(A_gp[0][:, :], 'True location->observation mapping')
 
 # print(pA_history)
 if learning_B:
-    for a in range(agent.num_controls[0]): #loop over actions
+    for u in range(agent.num_controls[0]): #loop over actions
         #plot_likelihood(B_gm[0][:, :,a], 'Initial beliefs about transition mapping under action {}'.format(location_observations[a]))
-        plot_likelihood(agent.B[0][:, :,a], 'Final beliefs about transition mapping under action {}'.format(location_observations[a])) # recall that when learning, agent.A is the expected value of the approximate posterior over A.
+        plot_likelihood(agent.B[0][:, :, u], 'Final beliefs about transition mapping under action {}'.format(location_observations[u])) # recall that when learning, agent.A is the expected value of the approximate posterior over A.
         #plot_likelihood(B_gp[0][:, :,a], 'True transition mapping under action {}'.format(location_observations[a]))
+
+if learning_D:
+    print(D_gm[0], 'Initial beliefs about initial state distribution')
+    print(agent.D[0], 'Final beliefs about initial state distribution') # recall that when learning, agent.D is the expected value of the approximate posterior over A.
